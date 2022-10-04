a03374f65cd916bac138431e7832a53ea756345f
vic-lee <96267768+vic-lee@users.noreply.github.com>
2022-10-03 21:22:03 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+//console.log('test')
 
+let randomPerson = Math.floor((Math.random()*82)+1);
+let starWarsURL = "https://swapi.dev/api/people/" + randomPerson;
+
+fetch(starWarsURL).then(response => response.json()).then(json => {
+    // console.log(json)
+
+    personName.innerHTML = json.name;
+    personHeight.innerHTML = json.height + ' cm';
+    haircolor.innerHTML = json.hair_color;
+   
+});
