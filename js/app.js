cc3fdba9d83abdc74ecacfeb09690cf5ac33ad76
vic-lee <96267768+vic-lee@users.noreply.github.com>
2022-10-05 20:57:42 -1000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //console.log('test')
 
-let randomPerson = Math.floor((Math.random()*82)+1);
+let randomPerson = Math.floor((Math.random()*82));
 let starWarsURL = "https://swapi.dev/api/people/" + randomPerson;
 
 fetch(starWarsURL).then(response => response.json()).then(json => {
