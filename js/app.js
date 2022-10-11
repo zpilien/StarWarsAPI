bdf0cc7171b427665d1f3f5ca9eb8cf9afb8a2f1
unknown <zpilien@gmail.com>
2022-10-10 17:42:03 -1000

    added height and birthyear stuck on ships

--- js/app.js
+++ js/app.js
@@ -6,6 +6,25 @@ let starWarsURL = "https://swapi.dev/api/people/" + randomPerson;
 fetch(starWarsURL).then(response => response.json()).then(json => {
     // console.log(json)
     personName.innerHTML = json.name;
- 
-   
+    console.log(json.starships);
+    console.log(json.height);
+
+let heightBox = document.createElement('div');
+heightBox.id = 'personHeight';
+heightBox.innerHTML = json.height + ' cm';
+personName.appendChild(heightBox);
+
+let yearBox = document.createElement('div');
+yearBox.id = 'yearBorn';
+yearBox.innerHTML = json.birth_year;
+personName.appendChild(yearBox);
+
+
+let shipBox = document.createElement('div');
+shipBox.id = 'personShip';
+for(i=0; i<shipBox.length; i++){
+    shipBox[i].innerHTML = json.starships;
+    personHeight.appendChild(shipBox[i]);
+}
+
 });
